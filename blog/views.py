b7a5daf90bcbdd641162f89ca558e3e91153211d
Andrew Muirhead <andrewtce10@gmail.com>
2022-03-12 20:52:56 -0500

    template support

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Andrew M',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 12, 2022'
+    },
+    {
+        'author': 'Sean Limon',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 13, 2022'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
+
