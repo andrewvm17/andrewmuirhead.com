e6d7ebe96bfce548ea7acd41d813242e04f14954
Andrew Muirhead <andrewtce10@gmail.com>
2022-03-16 13:57:50 -0400

    Heroku update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Personal website project started March 2022
-# Django Python Web Framework, HTML, CS and JS 
-# Personal project in prep for Bitcamp 2022
-# terpshockey.com precursor
-# tpnsl.com precursor
+# Django Python Web Framework, HTML, CSS
+ Personal project in prep for Bitcamp 2022
+ terpshockey.com precursor
+ tpnsl.com precursor
--- project_1/settings.py
+++ project_1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f4dvnlgi+&h&8@26!gih(i)#l#xc&)va4(o5ae!e6e(_uwa%6+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'project_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+#DATABASES = {
+  ##  'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   ## }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
